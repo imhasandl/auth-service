acb64b6a62fae67e3ed0d8578fc89bf6b2bbdba0
hasan <imhasandl04@gmail.com>
2025-04-15 21:38:03 +0300

    CI changes

--- cmd/helper/responseGRPC.go
+++ cmd/helper/responseGRPC.go
@@ -9,11 +9,13 @@ import (
 	"google.golang.org/grpc/status"
 )
 
+// RespondWithErrorGRPC creates a gRPC error response with the specified code and message
+// It logs the error if provided and returns a formatted gRPC status error
 func RespondWithErrorGRPC(ctx context.Context, code codes.Code, msg string, err error) error {
 	if err != nil {
 		log.Println(err)
 	}
-	
+
 	if code > codes.Internal { // 5XX equivalent in gRPC
 		log.Printf("Responding with 5XX gRPC error: %s", msg)
 	}
