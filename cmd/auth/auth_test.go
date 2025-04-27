62037a13c2fee481698b54303a969d10b093b355
hasan <imhasandl04@gmail.com>
2025-04-27 22:56:39 +0300

    mock tests template and test hashing and checking passwords

---
+++ cmd/auth/auth_test.go
@@ -0,0 +1,24 @@
+package auth
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestHashAndCheckPassword(t *testing.T){
+	password := "testPassword123"
+
+	// Test password hashing
+	hashedPassword, err := HashPassword(password)
+	assert.NoError(t, err)
+	assert.NotEqual(t, password, hashedPassword)
+
+	// Test correct password check
+	err = CheckPassword(hashedPassword, password)
+	assert.NoError(t, err)
+
+	// Test incorrect password check
+	err = CheckPassword(hashedPassword, "wrongPasswordExample")
+	assert.NoError(t, err)
+}
